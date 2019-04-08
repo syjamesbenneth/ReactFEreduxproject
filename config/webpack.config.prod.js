52b177e56bd10b2afd75b60d4163cd3197c9d9f6
Adrian King Legaspi <alegaspi@outlier.digital>
2019-04-08 18:34:46 +0800

    Modify config for production build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -16,8 +16,8 @@ const BundleTracker = require('webpack-bundle-tracker');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-// const publicPath = paths.servedPath;
-const publicPath = "/static/bundles/";
+ const publicPath = paths.servedPath;
+//const publicPath = "/static/bundles/";
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
 const shouldUseRelativeAssetPaths = publicPath === './';
